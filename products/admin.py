68756efa7196340e71b3cb46b3c74729b2c9189c
Dayana <dayana.nashkova@yahoo.com>
2023-10-22 16:50:58 +0100

    feat: create book models and migrate #6

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Author, Book, Category, Genre
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'genre', 'date_published']
+    list_filter = ['genre', 'date_published']
+    search_fields = ['title', 'author__name',]
+
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Book, BookAdmin)
--- products/models.py
+++ products/models.py
@@ -1,7 +1,90 @@
+import uuid
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 # Create your models here.
-class Product(models.Model):
-    title = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Genre(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(
+        null=True, blank=True, upload_to='authors/', default='authors/default-author.png')
+    bio = models.TextField()
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Book(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=255)
+    author = models.ManyToManyField(Author, related_name='books')
+    description = models.TextField()
+    image = models.ImageField(
+        null=True, blank=True, upload_to='books/', default='books/product-not-found.png')
+    publisher = models.CharField(max_length=200)
+    date_published = models.DateField()
+    language = models.CharField(max_length=200)
+    pages = models.IntegerField(null=True, blank=True)
+    isbn = models.CharField(max_length=13)
+    stock_amount = models.IntegerField(
+        default=1, validators=[MinValueValidator(0), MaxValueValidator(1000)])
+    in_stock = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    discount = models.IntegerField(null=True, blank=True)
+    sale_price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.title)
+
+    @property
+    def price_discount(self):
+        ''' 
+        Calculate discount based on new price 
+        '''
+        if self.sale_price:
+            discount = ((self.price - self.sale_price) / self.price) * 100
+            return round(discount)
+        else:
+            return None
+
+    @property
+    def product_in_stock(self):
+        ''' 
+        Determines if the product is in stock
+        based on the stock amount.
+        '''
+        if self.stock_amount >= 1:
+            return True
+        else:
+            return False
+
+    def save(self, *args, **kwargs):
+        self.discount = self.price_discount
+        self.in_stock = self.product_in_stock
+        super().save(*args, **kwargs)
