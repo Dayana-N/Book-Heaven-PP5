b6d9ab3122b2e1b43408a8abddab0f5e96827f99
Dayana <dayana.nashkova@yahoo.com>
2023-10-22 19:34:34 +0100

    feat: add product cards on home page  #5

--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from products.models import Book
+
 
 # Create your views here.
 def home(request):
     '''
     A view to render the home page
     '''
-    return render(request, 'home/index.html')
+    books = Book.objects.all().order_by('-created')
+
+    context = {
+        'books': books,
+    }
+    return render(request, 'home/index.html', context)
