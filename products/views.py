5a66a77c81d10afac5587d9e9efe9371104262c4
Dayana <dayana.nashkova@yahoo.com>
2023-10-23 18:25:17 +0100

    feat: add all products page and render books #6

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import Book
+
+
 # Create your views here.
+def all_products(request):
+    '''
+    Render all products on products page
+    '''
+    books = Book.objects.all().order_by('-created')
+
+    context = {
+        'books': books,
+    }
+    return render(request, 'products/products.html', context)
