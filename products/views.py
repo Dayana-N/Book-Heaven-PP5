6f04c3d6b69cb8740409b489560e171ea508466b
Dayana <dayana.nashkova@yahoo.com>
2023-10-25 12:46:57 +0100

    feat: refine products by category and genre #8

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 
-from .models import Book
+from .models import Book, Category, Genre
 
 
 # Create your views here.
@@ -11,8 +11,22 @@ def all_products(request):
     Render all products on products page
     '''
     books = Book.objects.all().order_by('-created')
+    query = None
+    genre = None
+    category = None
 
     if request.GET:
+        if 'genre' in request.GET:
+            search_genre = request.GET['genre'].split(',')
+            books = books.filter(genre__name__in=search_genre)
+            genre = Genre.objects.filter(name__in=search_genre)
+
+        if 'category' in request.GET:
+            search_category = request.GET['category'].split(',')
+            books = books.filter(
+                genre__category__name__in=search_category)
+            category = Category.objects.filter(name__in=search_category)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -25,6 +39,9 @@ def all_products(request):
 
     context = {
         'books': books,
+        'search_query': query,
+        'genre': genre,
+        'category': category
     }
     return render(request, 'products/products.html', context)
 
