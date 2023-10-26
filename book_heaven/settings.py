40e273a5ccdff33f144c51ce9044b511fe1f28fe
Dayana <dayana.nashkova@yahoo.com>
2023-10-26 17:48:27 +0100

    feat: add and display django messages

--- book_heaven/settings.py
+++ book_heaven/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 import os
 from pathlib import Path
 
 import dj_database_url
+from django.contrib.messages import constants as messages
 
 if os.path.exists("env.py"):
     import env
@@ -60,6 +60,13 @@ INSTALLED_APPS = [
 
     'storages',
 ]
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
