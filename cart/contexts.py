b1ae147ebd0d1b14b82627e88c9fe45aa8e48982
Dayana <dayana.nashkova@yahoo.com>
2023-10-26 22:08:34 +0100

    feat: add logic to display items in cart #18

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,4 +1,6 @@
-from decimal import Decimal
+from django.shortcuts import get_object_or_404
+
+from products.models import Book
 
 
 def cart_contents(request):
@@ -7,20 +9,30 @@ def cart_contents(request):
     '''
     delivery_threshold = 50
     delivery_fee = 12
-
-    bag_items = []
+    cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
 
-    if total < delivery_threshold:
-        free_delivery_delta = delivery_threshold - total
-    else:
+    for item_id, quantity in cart.items():
+        book = get_object_or_404(Book, pk=item_id)
+        total += quantity * book.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'book': book,
+        })
+
+    if total > delivery_threshold or total == 0:
         delivery_fee = 0
         free_delivery_delta = 0
+    else:
+        free_delivery_delta = delivery_threshold - total
 
     grand_total = total + delivery_fee
     context = {
-        'bag_items': bag_items,
+        'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
         'delivery_fee': delivery_fee,
