1f4f349356f77cfcc2914ff4dc834013fdf8487b
Dayana <dayana.nashkova@yahoo.com>
2023-11-05 00:54:43 +0000

    feat: create checkout page and render form #38

--- book_heaven/urls.py
+++ book_heaven/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('accounts/', include('allauth.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
 
 # set url for user uploaded content
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+
+from .forms import OrderForm
+
 
 # Create your views here.
+def checkout(request):
+    ''' Renders the checkout page '''
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, 'Your shopping cart is empty')
+        return redirect('all-products')
+
+    order_form = OrderForm()
+
+    context = {'order_form': order_form}
+    return render(request, 'checkout/checkout.html', context)
