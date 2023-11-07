6c0a64ba01b4bc174a37619c3c26cbdd83ab0cfb
Dayana <dayana.nashkova@yahoo.com>
2023-11-07 17:34:01 +0000

    feat: add stripe realtime validation on input #22

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,9 @@
+import stripe
 from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import redirect, render
+
+from cart.contexts import cart_contents
 
 from .forms import OrderForm
 
@@ -8,16 +11,31 @@ from .forms import OrderForm
 # Create your views here.
 def checkout(request):
     ''' Renders the checkout page '''
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, 'Your shopping cart is empty')
         return redirect('all-products')
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.')
 
     context = {
         'order_form': order_form,
-        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
-        'client_secret': settings.STRIPE_SECRET_KEY,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, 'checkout/checkout.html', context)
