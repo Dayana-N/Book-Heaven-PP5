dd34c2ee859fc3e324630e5e8c99b2977439b505
Dayana <dayana.nashkova@yahoo.com>
2023-11-08 21:57:38 +0000

    feat: add checkout form data caching #22

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,10 @@
+import json
+
 import stripe
 from django.conf import settings
 from django.contrib import messages
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import HttpResponse, get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 
 from cart.contexts import cart_contents
 from products.models import Book
@@ -11,6 +14,27 @@ from .models import Order, OrderLineItem
 
 
 # Create your views here.
+@require_POST
+def cache_checkout_data(request):
+    '''
+    Handles the modification of Stripe PaymentIntent
+    with cart data and user information.
+    '''
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
+
 def checkout(request):
     ''' Renders the checkout page '''
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -57,6 +81,8 @@ def checkout(request):
                         request, 'Unknown item in shopping cart. Please try again later.')
                     order.delete()
                     return redirect('view-cart')
+
+            request.session['save_info'] = 'save-info' in request.POST
             return redirect('checkout-success', order.order_number)
         else:
             messages.error(
