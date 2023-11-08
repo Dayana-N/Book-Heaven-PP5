5e908747d7ba76108540ef10614c3805ee49cdb5
Dayana <dayana.nashkova@yahoo.com>
2023-11-08 17:18:39 +0000

    feat: set up webhook handler #22

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<str:order_number>/',
          views.checkout_success, name='checkout-success'),
+    path('wh/', webhook, name='webhook'),
 ]
