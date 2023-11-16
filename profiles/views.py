884ae872397c9a0c045b3be730fcd1ddaf61803e
Dayana <dayana.nashkova@yahoo.com>
2023-11-16 18:42:14 +0000

    feat: add profie update form #16

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,30 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 
+from .forms import UserProfileForm
 from .models import UserProfile
 
 
 # Create your views here.
+@login_required
 def profile_page(request, pk):
+    ''' Displays the user profile '''
+
     profile = get_object_or_404(UserProfile, id=pk)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     context = {
         'profile': profile,
+        'form': form,
+        'orders': orders,
     }
     return render(request, 'profiles/profile.html', context)
