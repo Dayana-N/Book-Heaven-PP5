4cd56e0530852827e7bcd32ddea47736ae1178c8
Dayana <dayana.nashkova@yahoo.com>
2023-11-18 19:06:15 +0000

    fix: navbar html warnings

--- products/models.py
+++ products/models.py
@@ -19,9 +19,10 @@ class Genre(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
-        return str(self.name)
+        return str(self.friendly_name)
 
 
 class Author(models.Model):
