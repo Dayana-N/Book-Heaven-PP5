eb90fd882af817346a3f8627e0af57a524ec774f
Dayana <dayana.nashkova@yahoo.com>
2023-11-24 21:53:58 +0000

    feat: add revenue on admin dashboard #23

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.shortcuts import render
 
 from checkout.models import Order
@@ -11,9 +12,11 @@ def admin_dashboard(request):
     orders = Order.objects.all().order_by('-date')
     orders_count = orders.count()
     books_count = Book.objects.all().count()
-    users_count = UserProfile.objects.all().count()
+    total_grand_total = Order.objects.aggregate(
+        total_grand_total=Sum('grand_total'))
+    grand_total_sum = total_grand_total['total_grand_total']
     context = {'orders': orders,
                'orders_count': orders_count,
                'books_count': books_count,
-               'users_count': users_count, }
+               'total': grand_total_sum, }
     return render(request, 'admin_panel/admin_dashboard.html', context)
