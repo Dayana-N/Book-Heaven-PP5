00c5efa72c885f882de0ddf9d832ca0ab02c4ea8
Dayana <dayana.nashkova@yahoo.com>
2023-11-28 12:53:54 +0000

    feat: add custom widget for image input

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from .models import Author, Book
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
@@ -8,6 +9,9 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Book
         exclude = ('discount', 'in_stock',)
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
 
 
 class AuthorForm(forms.ModelForm):
