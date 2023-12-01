a597b731ec41c7577043c131bd00c038917cce56
Dayana <dayana.nashkova@yahoo.com>
2023-12-01 11:20:51 +0000

    test: add automated tests for profiles

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class CartPageTests(TestCase):
+    def test_cart_page(self):
+        '''
+        Test if shopping cart page can be accessed
+        and it returns the correct template
+        '''
+        response = self.client.get(reverse('view-cart'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cart/cart.html')
+        self.assertContains(
+            response, '<h3>Shopping Cart</h3>')
+        self.assertNotContains(response, 'Not on the page')
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,51 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+
+from profiles.models import UserProfile
+
 
 # Create your tests here.
+class ProfilePageTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        ''' Create test Profile'''
+        cls.user = User.objects.create(
+            username='TestUser',
+            password='1234pass',
+            email='test@user.com',
+            id='1',
+        )
+        cls.user_profile = UserProfile.objects.update(
+            default_phone_number='0894123456',
+            default_country='Ireland',
+            id='bae50a5c-ec55-48c7-a822-9ebbbe4f918a'
+        )
+
+    def test_if_user_profile_created(self):
+        '''
+        Test if user profile is created
+        '''
+        user_profile = UserProfile.objects.filter().last()
+        self.assertEqual(user_profile.user.username, 'TestUser')
+        self.assertEqual(user_profile.default_phone_number, '0894123456')
+        expected_id = uuid.UUID('bae50a5c-ec55-48c7-a822-9ebbbe4f918a')
+        self.assertEqual(
+            user_profile.id, expected_id)
+
+    def test_profile_page(self):
+        '''
+        Test if the profile page can be accessed
+        and it returns the correct template
+        '''
+
+        user = User.objects.get(username='TestUser')
+        self.client.force_login(user)
+        response = self.client.get(
+            reverse('profile', kwargs={'pk': user.userprofile.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
+        self.assertContains(
+            response, '<p class="text-start fs-3 fw-4">My Details:</p>')
